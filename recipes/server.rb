19264aefc5c1e7e6964962fdd23f543b21143ed1
“sohaib629” <sohaibanwar629@gmail.com>
2020-01-30 21:15:07 +0000

    Add host attributes into web server index.html through setup.rb

--- recipes/server.rb
+++ recipes/server.rb
@@ -3,7 +3,12 @@ package 'httpd' do
 end
 
 file '/var/www/html/index.html' do
-	content '<h1>First Chef Cookbook Apache Page!</h1>'
+	content "<h1>First Chef Cookbook Apache Page!</h1>
+	HOSTNAME: #{node['hostname']}
+        IPADDRESS: #{node['ipaddress']}
+        CPU: #{node['cpu']['0']['mhz']}
+        MEMORY: #{node['memory']['total']}
+"
 end
 
 service 'httpd' do
